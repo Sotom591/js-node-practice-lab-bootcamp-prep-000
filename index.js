909ebf678133c2e63ebea8936b4b1e1a6f5442d6
sotom591@gmail.com <sotom591@gmail.com>
2018-08-08 01:41:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
 var name = "Marissa"
 var height = "62"
 
-console.log (parseInt (height) +1)
+
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height)
+console.log(parseInt (height) +1)
